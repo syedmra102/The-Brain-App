6fe1fe63d31fa5c9c3e9973f6a3d7d78ac908cac
Syed Imran Shah <zada44919@gmail.com>
2025-10-12 20:52:05 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ with col2:
     st.write("### Login")  # form title
     with st.form("login_form"):
         username = st.text_input("Username")
-        st.write=('Please confirm your password must have one capial letter and one small letter and one number and must 7 chacters!!')
         password = st.text_input("Password", type="password")
+        st.caption("Password must contain at least 7 characters, one uppercase, one lowercase, and one number.")
         login_btn = st.form_submit_button("Login")
 
 
