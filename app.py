a52ad61abfca3f1ee6af356375961a3e2bac80c3
Syed Imran Shah <zada44919@gmail.com>
2025-10-13 01:13:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ col1, col2, col3 = st.columns([1, 3, 1])
 with col2:
     st.markdown("<h1 style='text-align: center;'> Sign In</h1>", unsafe_allow_html=True)
 
-   with st.container():
-       with st.form("login_form"):
-            username = st.text_input("Username")
-            password = st.text_input("Password", type="password")
-            st.caption(" Password must contain at least 7 characters, one uppercase, one lowercase, and one number.")
-            login_btn = st.form_submit_button("Login")
+   
+    with st.form("login_form"):
+         username = st.text_input("Username")
+         password = st.text_input("Password", type="password")
+         st.caption(" Password must contain at least 7 characters, one uppercase, one lowercase, and one number.")
+         login_btn = st.form_submit_button("Login")
 
 # Form validation
 if 'login_btn' in locals() and login_btn:
