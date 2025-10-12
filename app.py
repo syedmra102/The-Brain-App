f75b0ac0b84e8ddcfa55e37a9b3edaf4566bbd81
Syed Imran Shah <zada44919@gmail.com>
2025-10-13 00:07:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Login", layout="centered")
 
 
 
-st.markdown("<h1 style='text-align: center; color: white;'>𝓣𝓱𝓮 𝓑𝓻𝓪𝓲𝓷 𝓐𝓹𝓹</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>The Brain App</h1>", unsafe_allow_html=True)
 
 
 # Center the form in the page
@@ -15,7 +15,7 @@ col1, col2, col3 = st.columns([1, 3, 1])  # middle column narrower
 
 with col2:
 
-    st.markdown("<h1 style='text-align: center; color: white;'>👉 𝓛𝓸𝓰𝓲𝓷</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: white;'>👉 Login </h1>", unsafe_allow_html=True)
 
     with st.container():
 
