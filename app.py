271d39692f65936669e3b1c5d245a2c7870cc7a0
Syed Imran Shah <zada44919@gmail.com>
2025-10-13 01:11:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.markdown("<h1 style='text-align: center;'> The Brain App</h1>", unsafe_allow_
 col1, col2, col3 = st.columns([1, 3, 1])
 
 with col2:
-st.markdown("<h1 style='text-align: center;'> Sign In</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'> Sign In</h1>", unsafe_allow_html=True)
 
    with st.container():
        with st.form("login_form"):
